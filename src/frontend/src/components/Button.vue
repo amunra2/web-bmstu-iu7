381028b5ef1c5d665df8ff8dc62922f92f7d0711
MyMiDiII <mymidi.student@yandex.ru>
2022-12-22 22:26:42 +0300

    lab_03: добавлен компонент кнопки

---
+++ src/frontend/src/components/Button.vue
@@ -0,0 +1,43 @@
+<template>
+  <button class="button">
+    <PinkText fontSize="var(--little-text)">
+        <slot></slot>
+    </PinkText>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import PinkText from '@/components/PinkText.vue'
+
+export default defineComponent({
+  data() {
+    return {
+      hover: false,
+    }
+  },
+  name: "Button",
+  components: {
+    PinkText
+  }
+})
+</script>
+
+<style scoped>
+.button{
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  background-color: transparent; 
+  border: 4px solid var(--magenta);
+  filter: drop-shadow(0px 0px 15px var(--magenta));
+  border-radius: 0px 0px 0px 15px;
+  font-family: "Chakra Petch";
+}
+.button:hover{
+  border: 4px solid var(--magenta);
+  border-radius: 0px 0px 0px 15px;
+  filter: drop-shadow(0px 0px 15px var(--cyan));
+}
+</style>
--- src/frontend/src/views/HomeView.vue
+++ src/frontend/src/views/HomeView.vue
@@ -1,19 +1,27 @@
 <template>
   <PinkText fontSize="var(--tiny-text)">Good</PinkText>
-  <PinkText fontSize="var(--little-text)">Good</PinkText>
+  <PinkText fontSize="var(--little-text)">Info</PinkText>
   <PinkText fontSize="var(--middle-text)">Good</PinkText>
   <PinkText fontSize="var(--large-text)">Good</PinkText>
   <PinkText fontSize="var(--huge-text)">Good</PinkText>
+  <Button v-on:click="consoleInfo">button</Button>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import PinkText from "@/components/PinkText.vue"
+import Button from "@/components/Button.vue"
 
 export default defineComponent({
   name: "HomeView",
   components: {
-    PinkText
+    PinkText,
+    Button
+  },
+  methods: {
+    consoleInfo: function(event: Event){
+      console.log(event.timeStamp)
+    },
   },
 });
 </script>
