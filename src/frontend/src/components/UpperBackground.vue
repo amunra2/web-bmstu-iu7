ed425a1d0ab2d756ccc591193695ef64f24a17ee
MyMiDiII <mymidi.student@yandex.ru>
2022-12-23 14:54:08 +0300

    lab_03: добавлен верхних прозрачный черный фон

---
+++ src/frontend/src/components/UpperBackground.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="wrapper upper-background">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: "UpperBackground"
+})
+</script>
+
+<style scoped>
+.upper-background {
+  background: rgba(0, 0, 0, 0.6);
+  box-shadow: 0px 0px 60px var(--cyan);
+}
+</style>
--- src/frontend/src/views/HomeView.vue
+++ src/frontend/src/views/HomeView.vue
@@ -1,12 +1,15 @@
 <template>
-  <PinkText fontSize="var(--tiny-text)">Good</PinkText>
-  <PinkText fontSize="var(--little-text)">Info</PinkText>
-  <PinkText fontSize="var(--middle-text)">Good</PinkText>
-  <PinkText fontSize="var(--large-text)">Good</PinkText>
-  <PinkText fontSize="var(--huge-text)">Good</PinkText>
-  <Button v-on:click="consoleInfo">LongLongLongLongLongLong</Button>
-  <UserPatch v-on:click="consoleInfo">User Name</UserPatch>
-
+  <body class="container">
+    <UpperBackground class="center">
+      <PinkText fontSize="var(--tiny-text)">Good</PinkText>
+      <PinkText fontSize="var(--little-text)">Info</PinkText>
+      <PinkText fontSize="var(--middle-text)">Good</PinkText>
+      <PinkText fontSize="var(--large-text)">Good</PinkText>
+      <PinkText fontSize="var(--huge-text)">Good</PinkText>
+      <Button v-on:click="consoleInfo">LongLongLongLongLongLong</Button>
+      <UserPatch v-on:click="consoleInfo">User Name</UserPatch>
+    </UpperBackground>
+  </body>
 </template>
 
 <script lang="ts">
@@ -14,13 +17,15 @@ import { defineComponent } from "vue";
 import PinkText from "@/components/PinkText.vue"
 import Button from "@/components/Button.vue"
 import UserPatch from "@/components/UserPatch.vue"
+import UpperBackground from "@/components/UpperBackground.vue";
 
 export default defineComponent({
   name: "HomeView",
   components: {
     PinkText,
     Button,
-    UserPatch
+    UserPatch,
+    UpperBackground
   },
   methods: {
     consoleInfo: function(event: Event){
@@ -29,3 +34,21 @@ export default defineComponent({
   },
 });
 </script>
+
+<style scoped>
+.container {
+  display: flex;
+  margin: 0;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+}
+.center {
+  position: relative;
+  width: 75%;
+  display: grid;
+  justify-content: center;
+  align-items: center;
+}
+</style>
